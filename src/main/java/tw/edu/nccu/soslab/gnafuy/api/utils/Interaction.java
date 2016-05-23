9f60396735784c50b9b4709925e12c2424fd4cc9
jjchen <jjchen@jjchende-MacBook-Pro.local>
2016-05-24 01:22:03 +0800

    update utils

---
+++ src/main/java/tw/edu/nccu/soslab/gnafuy/api/utils/Interaction.java
@@ -0,0 +1,54 @@
+package tw.edu.nccu.soslab.gnafuy.api.utils;
+
+/**
+ * The descendant of Interaction offers abilities to:
+ * 1. convert type T to string
+ * 2. convert string to instance with type T
+ * Created by jjchen on 2016/5/24.
+ */
+public abstract class Interaction<T> {
+    protected final T value;
+    protected final String valueAsString;
+    /**
+     * queue: to indicate this interaction should go after processing, optional.
+     */
+    protected String queue = null;
+
+    public Interaction(T value) {
+        this.value = value;
+        this.valueAsString = getStringFromValue(value);
+    }
+
+    public Interaction(String valueAsString) {
+        this.valueAsString = valueAsString;
+        this.value = getValueFromString(valueAsString);
+    }
+
+    /**
+     * Convert specific string to corresponding instance with type T
+     * @param from
+     * @return instance of type T
+     */
+    abstract protected T getValueFromString(String from);
+
+    /**
+     * Convert instance with type T to specific string
+     * @param from
+     * @return specific string
+     */
+    abstract protected String getStringFromValue(T from);
+
+    public abstract String getKey();
+
+    public T getCopyOfValue() {
+        return getValueFromString(valueAsString);
+    }
+
+    public String getValueAsString() {
+        return valueAsString;
+    }
+
+    public String getQueue() {
+        return queue;
+    }
+}
